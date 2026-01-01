c6d4e35b6cb4cba605687be0b98163c6d9a8578f
Sathvic Sharma <sathvic1306@gmail.com>
2026-01-01 04:57:25 +0000

    🚀 Sync LeetCode submission: Runtime - 0 ms (100.00%), Memory - 11.3 MB (95.38%)

--- submissions/0066-plus-one/solution.cpp
+++ submissions/0066-plus-one/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) 
     {
-        int n = digits.size();        
-        for (int i = n - 1; i >= 0; i--) 
+        int n = digits.size()-1;        
+        for (int i=n;i>=0;i--) 
         {
-            if (digits[i] < 9) {
+            if (digits[i]<9) {
                 digits[i]++;
                 return digits;
             }            
-            digits[i] = 0;
+            digits[i]=0;
         }       
         digits.insert(digits.begin(), 1);
         return digits;
