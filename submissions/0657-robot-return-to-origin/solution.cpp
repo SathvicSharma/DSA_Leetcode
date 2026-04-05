7256234422e767d55e2831e41433ac7897d480a4
Sathvic Sharma <sathvic1306@gmail.com>
2026-04-05 12:59:19 +0000

    🚀 Sync LeetCode submission: Runtime - 2 ms (50.83%), Memory - 10.2 MB (92.36%)

---
+++ submissions/0657-robot-return-to-origin/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+        for(int i=0;i<moves.size();i++)
+        {
+            if(moves[i]=='U')
+                x++;
+            else if(moves[i]=='D')
+                x--;
+            else if(moves[i]=='L')
+                y++;
+            else
+                y--;
+        }
+        if(!x&!y)
+            return true;
+        else
+            return false;
+    }
+};
