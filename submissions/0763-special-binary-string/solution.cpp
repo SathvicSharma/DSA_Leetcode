4173531a210cfe6e52acc371d0c0a093eceb54f4
Sathvic Sharma <sathvic1306@gmail.com>
2026-04-09 07:49:23 +0000

    🚀 Sync LeetCode submission: Runtime - 3 ms (21.16%), Memory - 9.4 MB (30.57%)

---
+++ submissions/0763-special-binary-string/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> subs;
+        int count = 0, start = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') 
+                count++;
+            else 
+                count--;
+            if (count == 0) {
+                string inner = makeLargestSpecial(s.substr(start + 1, i - start - 1));
+                subs.push_back("1" + inner + "0");
+                start=i+1;
+            }
+        }
+        sort(subs.begin(), subs.end(), greater<string>());
+        string result = "";
+        for (int i=0;i<subs.size();i++)
+            result +=subs[i];
+        return result;
+    }
+};
