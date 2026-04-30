630c941b08fb626bffc575b22d286c0262642b6a
Sathvic Sharma <sathvic1306@gmail.com>
2026-04-30 10:56:00 +0000

    🚀 Sync LeetCode submission: Runtime - 0 ms (100.00%), Memory - 29.6 MB (40.83%)

--- submissions/0189-rotate-array/solution.cpp
+++ submissions/0189-rotate-array/solution.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int>ans;
-        k=k%nums.size();
-        for(int i=nums.size()-k;i<nums.size();i++)
-        {
-            ans.push_back(nums[i]);
-        }
-        for(int i=0;i<nums.size()-k;i++)
-        {
-            ans.push_back(nums[i]);
-        }
-        nums=ans;
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
