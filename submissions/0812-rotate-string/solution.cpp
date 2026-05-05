a81ce620ef02101cfce6cac225f9b49cf37824f7
Sathvic Sharma <sathvic1306@gmail.com>
2026-05-05 04:55:22 +0000

    🚀 Sync LeetCode submission: Runtime - 0 ms (100.00%), Memory - 8.1 MB (59.72%)

---
+++ submissions/0812-rotate-string/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())
+            return false;
+        string check=s+s;
+        if(check.find(goal)!=string::npos)
+            return true;
+        else
+            return false;
+    }
+};
