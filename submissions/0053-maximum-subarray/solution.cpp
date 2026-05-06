934732c69c64dcc7c5d6e4ec4f4d68a41507a67a
Sathvic Sharma <sathvic1306@gmail.com>
2026-05-06 10:15:27 +0000

    🚀 Sync LeetCode submission: Runtime - 0 ms (100.00%), Memory - 71.6 MB (98.11%)

---
+++ submissions/0053-maximum-subarray/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum=INT_MIN,sum=0;
+        for(int val:nums)
+        {
+            sum+=val;
+            max_sum=max(max_sum,sum);
+            if(sum<0)
+                sum=0;
+        }
+        return max_sum;
+    }
+};
