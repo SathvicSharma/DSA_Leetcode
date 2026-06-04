4eba467b5e9af4bc51863043c9ef256cfb38173b
Sathvic Sharma <sathvic1306@gmail.com>
2026-06-04 07:38:56 +0000

    🚀 Sync LeetCode submission: - Jump Game II - Runtime - 0 ms (100.00%), Memory - 20.6 MB (33.63%)

---
+++ submissions/0045-jump-game-ii/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+        while(right<nums.size()-1)
+        {
+            int farthest=right;
+            for(int i=left;i<=right;i++)
+            {
+                farthest=max(farthest,i+nums[i]);
+            }
+            left=right+1;
+            right=farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
