2ce0f190b154a3815b0b8de9a96fc83cce860e12
Sathvic Sharma <sathvic1306@gmail.com>
2026-06-06 04:19:00 +0000

    🚀 Sync LeetCode submission: - Find the Middle Index in Array - Runtime - 0 ms (100.00%), Memory - 16.1 MB (32.93%)

---
+++ submissions/2102-find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+         int right_sum=0;
+        for(int val:nums)
+            right_sum+=val;
+        int left_sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            right_sum-=nums[i];
+            if(left_sum==right_sum)
+                return i;
+            left_sum+=nums[i];
+        }
+        return -1;
+    }
+};
