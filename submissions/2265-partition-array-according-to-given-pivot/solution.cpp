13ff350cb855141009c1dd776d87d834e7d38e47
Sathvic Sharma <sathvic1306@gmail.com>
2026-06-08 04:36:58 +0000

    🚀 Sync LeetCode submission: - Partition Array According to Given Pivot - Runtime - 10 ms (34.56%), Memory - 138.1 MB (27.75%)

--- submissions/2265-partition-array-according-to-given-pivot/solution.cpp
+++ submissions/2265-partition-array-according-to-given-pivot/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>lside,rside,pivot_all;
+        vector<int>lesser,greater,piv;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]<pivot)
+            if(pivot<nums[i])
+                greater.push_back(nums[i]);
+            else if(pivot>nums[i])
+                lesser.push_back(nums[i]);
+            else
+                piv.push_back(nums[i]);
+        }
+        int l=0,g=0,p=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(l<lesser.size())
             {
-                lside.push_back(nums[i]);
+                nums[i]=lesser[l++];
             }
-            else if(nums[i]>pivot)
+            else if(p<piv.size())
             {
-                rside.push_back(nums[i]);
+                nums[i]=piv[p++];
             }
             else
-            {
-                pivot_all.push_back(nums[i]);
-            }
+                nums[i]=greater[g++];
         }
-        lside.insert(lside.end(), pivot_all.begin(), pivot_all.end());
-        lside.insert(lside.end(), rside.begin(), rside.end());
-        
-        return lside;
+        return nums;
     }
 };
